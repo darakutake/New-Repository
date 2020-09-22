320782b2559a9f7b0f218550b303a8ec8ed16b28
darakutake <44940319+darakutake@users.noreply.github.com>
2020-09-22 13:17:32 +0900

    Update and rename test.cs to screenshotNote.cs

--- screenshotNote.cs
+++ screenshotNote.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
  
- IEnumerator Trimming()
+ IEnumerator screenshot()
     {
         //↓コレ大事
         yield return new WaitForEndOfFrame();
 
-        Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, true);
         texture.ReadPixels(new Rect(0, 0, texture.width, texture.height), 0, 0);
         texture.Apply();
         yield break;
